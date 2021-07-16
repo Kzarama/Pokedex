029fa89c7bb8b7014fdf1d64a2a34c9f29e78463
kzarama <zaramaluna1999@hotmail.com>
2021-07-16 18:00:46 -0500

    template added

---
+++ src/templates/PokedexTemplate.tsx
@@ -0,0 +1,47 @@
+import './PokedexTemplate.sass';
+
+// Components
+import HeaderPokedex from '../components/organisms/HeaderPokedex/HeaderPokedex';
+import MainPokedex from '../components/organisms/MainPokedex/MainPokedex';
+import OtherPokemons from '../components/molecules/OtherPokemons/OtherPokemons';
+
+// Pokedex template layout
+function PokedexTemplate(props: {
+  imageHeader: string;
+  altImageHeader: string;
+  imageTypeMain: string;
+  altImageTypeMain: string;
+  textNamePokemonMain: string;
+  imagePokemonMain: string;
+  stats: {
+    number: string;
+    level: string;
+    type: string;
+    ability: string;
+    height: string;
+    weight: string;
+  };
+  footerImages: {
+    image: string;
+    altImage: string;
+  }[];
+}) {
+  return (
+    <div className="Pokedex__container">
+      <HeaderPokedex
+        image={props.imageHeader}
+        altImage={props.altImageHeader}
+      />
+      <MainPokedex
+        image={props.imageTypeMain}
+        altImage={props.altImageTypeMain}
+        text={props.textNamePokemonMain}
+        imagePokemon={props.imagePokemonMain}
+        stats={props.stats}
+      />
+      <OtherPokemons images={props.footerImages} />
+    </div>
+  );
+}
+
+export default PokedexTemplate;
