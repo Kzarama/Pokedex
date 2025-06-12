b11917884b83984b14c137842cb909eb824ea782
kevin zarama <kevin.zarama@pragma.com.co>
2025-06-12 10:32:20 -0500

    firestore added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'pokedex-app-kz',
+        appId: '1:1008231510677:web:5eecae2e0ba6f09905219c',
+        storageBucket: 'pokedex-app-kz.firebasestorage.app',
+        apiKey: 'AIzaSyBzVDTF-sP49XdSekNWW5ljKFA1vP62P4M',
+        authDomain: 'pokedex-app-kz.firebaseapp.com',
+        messagingSenderId: '1008231510677',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
