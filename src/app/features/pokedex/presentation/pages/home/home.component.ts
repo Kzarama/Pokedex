be131bd867afbae9743714bb6cbd2b128d74c0ff
kevin zarama <kevin.zarama@pragma.com.co>
2025-06-13 23:14:02 -0500

    - loading page added

--- src/app/features/pokedex/presentation/pages/home/home.component.ts
+++ src/app/features/pokedex/presentation/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { GetPokemonsUseCase } from 'features/pokedex/application/use-cases/get-pokemons.usecase';
 import { HOME_TITLE } from 'features/pokedex/domain/constants/constants';
 import { Pokemon } from 'features/pokedex/domain/entities/pokemon.model';
+import { LoadingComponent } from '../../components/atoms/loading/loading.component';
 import { PokemonCardComponent } from '../../components/molecules/pokemon-card/pokemon-card.component';
 import { NotificationAdapterService } from '../../shared/notification.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PokemonCardComponent],
+  imports: [CommonModule, PokemonCardComponent, LoadingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
